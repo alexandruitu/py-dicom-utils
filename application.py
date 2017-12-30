f24fec2007c022c2a336ecb5f5617f4900246724
Alexandru Itu <alexandru.itu@gmail.com>
2017-12-30 15:23:47 +0200

    statistics endpoint

--- application.py
+++ application.py
@@ -1,14 +1,47 @@
-from flask import Flask
+from flask import Flask, request, abort
 from joblib import delayed
 import SimpleITK as sitk
 import dicom
+import json
 
 application = Flask(__name__)
 
 
-@application.route("/")
-def hello_world():
+# mean
+def mean(numbers):
+    return sum([float(i) for i in numbers]) / len(numbers)
+
+
+def median(numbers):
+    length = len(numbers)
+    numbers.sort()
+    if length % 2 == 0:
+        return str((float(numbers[length/2 - 1]) + float(numbers[length/2])) / 2)
+    else:
+        return numbers[length/2]
+
+
+@application.route('/')
+def heart_beat():
     return 'heart beat'
+
+
+@application.route('/<operation>', methods=['POST'])
+def statistics(operation):
+    try:
+        numbers = request.get_json()['numbers']
+    except Exception as e:
+        print(e)
+        return abort(500)
+    if operation == 'mean':
+        return str(mean(numbers))
+    elif operation == 'median':
+        return median(numbers)
+    elif operation == 'sort':
+        numbers.sort()
+        return json.dumps(numbers)
+    elif operation == 'mode':
+        return operation
 
 
 if __name__ == "__main__":
